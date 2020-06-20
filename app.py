06b48bca4128a545687d47578218e6c818870980
oluwasayo01 <oluwasayoakinkunmi@gmail.com>
2020-06-20 10:20:14 +0100

    created chatbot

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, stream_with_context
+# from chatbot import chatbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+with open('train.txt') as f:
+    conversation = f.readlines()
+
+chatbot = ChatBot('Jarvis',
+                  logic_adapters=['chatterbot.logic.MathematicalEvaluation',
+                                  'chatterbot.logic.TimeLogicAdapter',
+                                  'chatterbot.logic.BestMatch'])
+
+
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/chatbot/', methods=['POST'])
 def home():
-     return render_template('index.html')
+     # txt = text
+     if request.method == 'POST':
+          trainer = ListTrainer(chatbot)
+          trainer.train(conversation)
+          txt = str(request.form['message'])
+          resp = stream_with_context(chatbot.get_response(txt))
+          return Response(resp, status=200, mimetype='application/json')
+          
 
 
 if __name__ == '__main__':
--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,24 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ListTrainer
 
-conversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
+# with open('train.txt') as f:
+#     conversation = f.readlines()
 
-
-chatbot = ChatBot('Jarvis')
-
-trainer = ListTrainer(chatbot)
-
-trainer.train(conversation)
+# # conversation = [
+# #     "Hello",
+# #     "Hi there!",
+# #     "How are you doing?",
+# #     "I'm doing great.",
+# #     "That is good to hear",
+# #     "Thank you.",
+# #     "You're welcome."
+# # ]
 
 
-response = chatbot.get_response("That is good to hear")
-print(response)
+# chatbot = ChatBot('Jarvis',
+#                   logic_adapters=['chatterbot.logic.MathematicalEvaluation',
+#                                   'chatterbot.logic.TimeLogicAdapter',
+#                                   'chatterbot.logic.BestMatch'])
+
+# trainer = ListTrainer(chatbot)
+# trainer.train(conversation)
